ca7ee29997b9375490fae259a8139c3167502125
WHAT <ygyshka0@gmail.com>
2023-09-10 16:09:17 +0700

    add migrations

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
+from catalog.models import Product, Category
+
+
 # Register your models here.
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price', 'category_id')
+    list_filter = ('category_id',)
+    search_fields = ('name', 'description')
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
