a847d76a17e2bdfef5ae6f1cc9730f06d28e74cf
WHAT <ygyshka0@gmail.com>
2023-09-16 16:36:50 +0700

    change url

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from catalog.views import contacts, home, product
+from catalog.views import ContactsTemplateView, home, product
 from catalog.apps import CatalogConfig
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path('', home, name='home'),
-    path('contacts/', contacts, name='contacts'),
-    path('/product/<int:pk>', product, name='product'),
+    path('contacts/', ContactsTemplateView.as_view(), name='contacts'),
+    path('product/<int:pk>', product, name='product'),
 ]
