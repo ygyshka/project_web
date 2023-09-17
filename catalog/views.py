8e30b3991d4e47cf4f7a3fac94a6a97c3b5fe997
WHAT <ygyshka0@gmail.com>
2023-09-17 15:48:05 +0700

    change old homeworks

--- catalog/views.py
+++ catalog/views.py
@@ -1,39 +1,27 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView, DetailView
 
 from catalog.models import Product
 
 
 # Create your views here.
 
-# def contacts(request):
-#     print(request.method)
-#     if request.method == "POST":
-#         name = request.POST.get('name')
-#         phone = request.POST.get('phone')
-#         message = request.POST.get('message')
-#         print(f'{name} ({phone}): {message}')
-#
-#     return render(request, 'catalog/contacts.html')
+def contacts(request):
+    print(request.method)
+    if request.method == "POST":
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        print(f'{name} ({phone}): {message}')
 
-class ContactsTemplateView(TemplateView):
-    template_name = 'catalog/contacts.html'
+    return render(request, 'catalog/contacts.html')
 
 
-def home(request):
-    product_list = Product.objects.all()
-    context = {
-        'product_list': product_list,
-    }
-    return render(request, 'catalog/home.html', context)
+class HomeListView(ListView):
+    model = Product
+    template_name = 'catalog/home.html'
 
 
-def product(request, pk):
-    product_ = Product.objects.get(id=pk)
-    context = {
-        'object': product_,
-    }
-
-    return render(request, 'catalog/product.html', context)
-
-
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'catalog/product.html'
