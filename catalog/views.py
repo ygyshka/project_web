f45fec111f339cba5a42dd9314583a176a6865b8
WHAT <ygyshka0@gmail.com>
2023-09-29 23:27:29 +0700

    edit exercise 22.1 part 1

--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, ListView, DetailView
+from django.urls import reverse_lazy
 
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+
+from catalog.forms import ProductForm
 from catalog.models import Product
 
 
@@ -25,3 +28,20 @@ class HomeListView(ListView):
 class ProductDetailView(DetailView):
     model = Product
     template_name = 'catalog/product.html'
+
+
+class ProductCreateView(CreateView):
+    model = Product
+    form_class = ProductForm
+    success_url = reverse_lazy('catalog:home')
+
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    form_class = ProductForm
+    success_url = reverse_lazy('catalog:home')
+
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    success_url = reverse_lazy('catalog:home')
