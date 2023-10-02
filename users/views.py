b0f0111efd101c0b71fffd184d460d03271d9df1
WHAT <ygyshka0@gmail.com>
2023-10-02 11:43:32 +0700

    start users login/logout

--- users/views.py
+++ users/views.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LogoutView
 from django.shortcuts import render
 
 # Create your views here.
+
+
+class UserLoginView(LoginView):
+    template_name = 'users/login.html'
+
+
+class UserLogoutView(LogoutView):
+    pass
