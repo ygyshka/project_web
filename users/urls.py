9d02182c718b6580c204ba1d8880ebcf4dcca652
WHAT <ygyshka0@gmail.com>
2023-10-03 15:38:26 +0700

    create registration and verification

--- users/urls.py
+++ users/urls.py
@@ -1,12 +1,23 @@
 from django.urls import path
+from django.views.generic import TemplateView
 
 from users.apps import UsersConfig
-from users.views import UserLoginView, UserLogoutView, UserRegisterView
+from users.views import UserLoginView, UserLogoutView, Register, EmailVerify
 
 app_name = UsersConfig.name
 
 urlpatterns = [
     path('', UserLoginView.as_view(), name='login'),
     path('logout/', UserLogoutView.as_view(), name='logout'),
-    path('register/', UserRegisterView.as_view(), name='register')
+    # path('register/', UserRegisterView.as_view(), name='register'),
+    path('register/', Register.as_view(), name='register'),
+    path('confirm_email/', TemplateView.as_view(template_name='users/confirm_email.html'), name='confirm_email'),
+
+    path('invalid_verify/', TemplateView.as_view(template_name='users/invalid_verify.html'), name='invalid_verify'),
+
+    path(
+        'veryfi_email/<uidb64>/<token>/',
+        EmailVerify.as_view(),
+        name='verify_email',
+    ),
 ]
